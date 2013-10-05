e9dea6970f120daf1567787e031f1f7ae71b73ea
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-10-05 13:54:01 +0000

    Update version numbers to 0.0.3

--- patched-up-contact-card-widget.php
+++ patched-up-contact-card-widget.php
@@ -3,7 +3,7 @@
 /* Plugin Name: Patched Up Contact Card 
  * Plugin URI: http://patchedupcreative.com/plugins/contact-card
  * Description: A very clean and semantic way of adding social networking links to your sidebar
- * Version: 0.0.2
+ * Version: 0.0.3
  * Date: 10-04-2013
  * Author: Casey Patrick Driscoll
  * Author URI: http://caseypatrickdriscoll.com
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://caseypatrickdriscoll.com
 Tags: widget, social media, contact
 Requires at least: 3.6.1
 Tested up to: 3.6.1
-Stable tag: 0.0.2
+Stable tag: 0.0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
